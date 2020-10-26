daed71fdc26f0aaa64ba0d768efb13c29796f64a
MateoMatta <mateomatta14@hotmail.com>
2020-10-26 12:46:34 -0500

    Fixing the provisioner playbooks of Ansible

--- scripts/glusterfs.sh
+++ scripts/glusterfs.sh
@@ -1,12 +1,18 @@
-yum install -y centos-release-gluster
-yum install -y glusterfs-server
-service glusterd start
+sudo yum install -y centos-release-gluster
+sudo yum install -y glusterfs-server
+sudo service glusterd start
 
-sfdisk /dev/sdb << EOF
+sudo sfdisk /dev/sdb << EOF
 ;
 EOF
 
 mkfs.xfs /dev/sdb1
 mkdir -p /gluster/data 
-mount /dev/sdb1 /gluster/data/
+sudo mount /dev/sdb1 /gluster/data/
+
+sudo yum install yum-utils -y
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install -y docker-ce
+sudo systemctl start docker
+sudo systemctl enable docker
 
