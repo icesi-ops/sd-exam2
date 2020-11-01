e8913f35445ccedf4437d9328062908ed1ef8a0d
MateoMatta <mateomatta14@hotmail.com>
2020-11-01 04:55:34 -0500

    Fixed Ansible provisioning and configuration

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Update package list
-sudo yum update
+sudo yum update -y
 
 # Install dependencies
 sudo yum install -y sshpass
-curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
+sudo curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
 sudo python get-pip.py
 sudo -H pip install --upgrade pip
 
@@ -13,8 +13,8 @@ sudo -H pip install --upgrade pip
 sudo -H pip install ansible
 
 # Run Ansible playbooks
-echo -e "\nRUNNING ANSIBLE [cluster.yml] **************************************************\n"
-ansible-playbook -i ansible_hosts install.yml
+echo -e "\nRUNNING ANSIBLE [install.yml] **************************************************\n"
+ansible-playbook -i hosts install.yml
 echo -e "\nRUNNING ANSIBLE [master.yml] ***************************************************\n"
 ansible-playbook -i hosts master.yml
 echo -e "\nRUNNING ANSIBLE [join.yml] *****************************************************\n"
--- scripts/glusterfs.sh
+++ scripts/glusterfs.sh
@@ -2,11 +2,11 @@ sudo yum install -y centos-release-gluster
 sudo yum install -y glusterfs-server
 sudo service glusterd start
 
-sudo sfdisk /dev/sdb << EOF
+sudo sfdisk --force /dev/sdb << EOF
 ;
 EOF
 
-mkfs.xfs /dev/sdb1
-mkdir -p /gluster/data 
-sudo mount /dev/sdb1 /gluster/data/
+# mkfs.xfs /dev/sdb1
+# mkdir -p /gluster/data 
+# sudo mount /dev/sdb1 /gluster/data/
 
