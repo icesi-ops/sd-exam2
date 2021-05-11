f2dfe48ac142311dd166054fa3b5fa2841377f8b
Franjaresc <Franjaresc@hotmail.com>
2021-05-11 10:19:04 -0500

    Finish the task-006

--- src/test/kotlin/ApplicationTest.kt
+++ src/test/kotlin/ApplicationTest.kt
@@ -26,8 +26,6 @@ class ApplicationTest {
             }
         }
     }
-
-
     @Test
     fun innerDBTest() {
         withTestEngine {
@@ -36,6 +34,143 @@ class ApplicationTest {
                 assertEquals(DefaultMovieList, Json.decodeFromString<List<Movie>>(response.content?:"[]").map {
                     it.copy(id = null)
                 })
+            }
+        }
+    }
+    @Test
+    fun movieGetErrorID() {
+        withTestEngine {
+            handleRequest(HttpMethod.Get, "/api/movie/a").apply {
+                assertEquals(HttpStatusCode.BadRequest, response.status())
+                assertEquals("Missing or malformed id",
+                    Json.decodeFromString<Map<String,String>>(response.content ?: "{}")["error"]
+                )
+            }
+        }
+    }
+    @Test
+    fun movieGetErrorMovieDoesntExist() {
+        withTestEngine {
+            handleRequest(HttpMethod.Get, "/api/movie/-1").apply {
+                assertEquals(HttpStatusCode.NotFound, response.status())
+                assertEquals("The Movie with id=-1 doesn't exist",
+                    Json.decodeFromString<Map<String,String>>(response.content ?: "{}")["error"]
+                )
+            }
+        }
+    }
+    @Test
+    fun createMovieTest() {
+        withTestEngine {
+            handleRequest(HttpMethod.Post, "/api/movie"){
+                addHeader("Content-Type", "application/json")
+                setBody("""{"name":"limitless","year":2011,"id":null}""")
+            }.apply {
+                println(response.content)
+                assertEquals(HttpStatusCode.OK, response.status())
+                assertEquals(Movie(name="limitless",year=2011), Json.decodeFromString<Map<String,List<Movie>>>(response.content?:"{}")["added"]!!.first().copy(id=null))
+            }
+        }
+    }
+    @Test
+    fun createErrorMovieTest() {
+        withTestEngine {
+            handleRequest(HttpMethod.Post, "/api/movie"){
+                addHeader("Content-Type", "application/json")
+                setBody("""{"name":"limitless","year":2011,"id"=null}""")
+            }.apply {
+                assertEquals(HttpStatusCode.BadRequest, response.status())
+                assertEquals("invalid request body",
+                    Json.decodeFromString<Map<String,String>>(response.content ?: "{}")["error"]
+                )
+            }
+        }
+    }
+    @Test
+    fun deleteMovieTest() {
+        withTestEngine {
+            handleRequest(HttpMethod.Get, "/api/movie").apply {
+                val movies = Json.decodeFromString<List<Movie>>(response.content ?: "[]")
+                handleRequest(HttpMethod.Delete, "/api/movie/${movies.first().id}").apply {
+                    println(response.content)
+                    assertEquals(HttpStatusCode.OK, response.status())
+                    assertEquals(movies.first(),
+                        Json.decodeFromString<Map<String,Movie>>(response.content ?: "{}")["deleted_movie"]
+                    )
+                }
+            }
+        }
+    }
+    @Test
+    fun updateMovieTest() {
+        withTestEngine {
+            handleRequest(HttpMethod.Get, "/api/movie").apply {
+                val movies = Json.decodeFromString<List<Movie>>(response.content ?: "[]")
+                handleRequest(HttpMethod.Post, "/api/movie/${movies.first().id}"){
+                    addHeader("Content-Type", "application/json")
+                    setBody("""{"name":"limitless","year":${movies.first().year},"id":null}""")
+                }.apply {
+                    println(response.content)
+                    assertEquals(HttpStatusCode.OK, response.status())
+                    assertEquals(movies.first().copy(name="limitless"),
+                        Json.decodeFromString<Movie>(response.content ?: "{}")
+                    )
+                }
+            }
+        }
+    }
+    @Test
+    fun updateErrorIDMovieTest() {
+        withTestEngine {
+            handleRequest(HttpMethod.Post, "/api/movie/a"){
+                addHeader("Content-Type", "application/json")
+                setBody("""{"name":"limitless","year":1999,"id":null}""")
+            }.apply {
+
+                assertEquals("Missing or malformed id",
+                    Json.decodeFromString<Map<String,String>>(response.content ?: "{}")["error"]
+                )
+            }
+        }
+    }
+    @Test
+    fun updateErrorBodyMovieTest() {
+        withTestEngine {
+            handleRequest(HttpMethod.Get, "/api/movie").apply {
+                val movies = Json.decodeFromString<List<Movie>>(response.content ?: "[]")
+                handleRequest(HttpMethod.Post, "/api/movie/${movies.first().id}"){
+                    addHeader("Content-Type", "application/json")
+                    setBody("""{"name":"limitless","year":${movies.first().year},"id"=null}""")
+                }.apply {
+                    assertEquals(HttpStatusCode.BadRequest, response.status())
+                    assertEquals("invalid request body",
+                        Json.decodeFromString<Map<String,String>>(response.content ?: "{}")["error"]
+                    )
+                }
+            }
+        }
+    }
+    @Test
+    fun deleteWithUnexistIDMovieTest() {
+        withTestEngine {
+            handleRequest(HttpMethod.Delete, "/api/movie/-1").apply {
+                println(response.content)
+                assertEquals(HttpStatusCode.OK, response.status())
+                assertEquals(null,
+                    Json.decodeFromString<Map<String,Movie?>>(response.content ?: "{}")["deleted_movie"]
+                )
+            }
+        }
+    }
+    @Test
+    fun deleteWithBadIDMovieTest() {
+        withTestEngine {
+            handleRequest(HttpMethod.Delete, "/api/movie/aaaa").apply {
+                println(response.content)
+                assertEquals(HttpStatusCode.BadRequest, response.status())
+                assertEquals("Missing or malformed id",
+                    Json.decodeFromString<Map<String,String>>(response.content ?: "{}")["error"]
+                )
             }
         }
     }
